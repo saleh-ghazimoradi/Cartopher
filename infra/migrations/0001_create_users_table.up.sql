f18676acff9b622dfbd6a86a7d4c9c2b62ecf44f
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-08 00:41:23 +0330

    fix a typo

--- infra/migrations/0001_create_users_table.up.sql
+++ infra/migrations/0001_create_users_table.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(100) NOT NULL,
     phone VARCHAR(20) NOT NULL,
     is_active BOOLEAN DEFAULT true,
-    rol user_role DEFAULT 'customer',
+    role user_role DEFAULT 'customer',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP WITH TIME ZONE
