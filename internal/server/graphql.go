89d9c130cfde3e0f087bf2cf3fa15585c410f6d8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-17 14:42:53 +0330

    make improvements to the graphql server

--- internal/server/graphql.go
+++ internal/server/graphql.go
@@ -1,121 +1,40 @@
 package server
 
 import (
-	"context"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gin-gonic/gin"
 	"github.com/saleh-ghazimoradi/Cartopher/graph"
 	"github.com/saleh-ghazimoradi/Cartopher/graph/resolver"
-	"github.com/saleh-ghazimoradi/Cartopher/internal/service"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
-type GraphQL struct {
-	authService    service.AuthService
-	userService    service.UserService
-	productService service.ProductService
-	cartService    service.CartService
-	orderService   service.OrderService
+type Graphql struct {
+	resolver *resolver.Resolver
 }
 
-type Option func(*GraphQL)
-
-func WithAuthService(authService service.AuthService) Option {
-	return func(g *GraphQL) {
-		g.authService = authService
-	}
-}
-
-func WithUserService(userService service.UserService) Option {
-	return func(g *GraphQL) {
-		g.userService = userService
-	}
-}
-
-func WithProductService(productService service.ProductService) Option {
-	return func(g *GraphQL) {
-		g.productService = productService
-	}
-}
-
-func WithCartService(cartService service.CartService) Option {
-	return func(g *GraphQL) {
-		g.cartService = cartService
-	}
-}
-
-func WithOrderService(orderService service.OrderService) Option {
-	return func(g *GraphQL) {
-		g.orderService = orderService
-	}
-}
-
-func (g *GraphQL) GraphqlHandler() *handler.Server {
-	rvr := resolver.NewResolver(
-		resolver.WithAuthService(g.authService),
-		resolver.WithUserService(g.userService),
-		resolver.WithProductService(g.productService),
-		resolver.WithCartService(g.cartService),
-		resolver.WithOrderService(g.orderService),
-	)
-
-	schema := graph.NewExecutableSchema(graph.Config{Resolvers: rvr})
-
+func (g *Graphql) Connect() *handler.Server {
+	schema := graph.NewExecutableSchema(graph.Config{Resolvers: g.resolver})
 	srv := handler.New(schema)
 
-	srv.AddTransport(transport.POST{})
-	srv.AddTransport(transport.GET{})
 	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
 	srv.AddTransport(transport.MultipartForm{})
 
 	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
 	srv.Use(extension.Introspection{})
 	srv.Use(extension.AutomaticPersistedQuery{
 		Cache: lru.New[string](100),
 	})
 
 	return srv
+
 }
 
-func (g *GraphQL) GraphqlHandlerWrapper() gin.HandlerFunc {
-	h := g.GraphqlHandler()
-	return func(ctx *gin.Context) {
-		h.ServeHTTP(ctx.Writer, ctx.Request)
+func NewGraphql(resolver *resolver.Resolver) *Graphql {
+	return &Graphql{
+		resolver: resolver,
 	}
-}
-
-func (g *GraphQL) PlaygroundHandler() gin.HandlerFunc {
-	h := playground.Handler("GraphQL playground", "/graphql/")
-	return func(ctx *gin.Context) {
-		h.ServeHTTP(ctx.Writer, ctx.Request)
-	}
-}
-
-func (g *GraphQL) GraphqlMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		userId, _ := ctx.Get("user_id")
-		userEmail, _ := ctx.Get("user_email")
-		userRole, _ := ctx.Get("user_role")
-
-		ct := context.WithValue(ctx.Request.Context(), "user_id", userId)
-		ct = context.WithValue(ctx.Request.Context(), "user_email", userEmail)
-		ct = context.WithValue(ctx.Request.Context(), "user_role", userRole)
-
-		ctx.Request = ctx.Request.WithContext(ct)
-
-		ctx.Next()
-	}
-}
-
-func NewGraphQL(opts ...Option) *GraphQL {
-	g := &GraphQL{}
-	for _, o := range opts {
-		o(g)
-	}
-	return g
 }
